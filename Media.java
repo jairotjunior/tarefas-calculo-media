14319d0b53a72f692b215056816b4a83c9cb10cb
JairoTJ <Jairotjunior@hotmail.com>
2024-05-14 08:54:42 -0300

    Finalizado Tarefa Calculadora Media

--- Media.java
+++ Media.java
@@ -1,7 +1,29 @@
 package Java.TarefaCalculoMedia;
+import java.util.Scanner;
 
 public class Media {
   public static void main(String[] args) {
-    
+    recebendoNotas();
+  }
+  
+  
+  public static void recebendoNotas () {
+
+    Scanner ler = new Scanner (System.in);
+    byte nota1, nota2, nota3, nota4;
+
+    System.out.printf("Digite a 1º nota: ");
+    nota1 = ler.nextByte();
+    System.out.printf("Digite a 2º nota: ");
+    nota2 = ler.nextByte();
+    System.out.printf("Digite a 3º nota: ");
+    nota3 = ler.nextByte();
+    System.out.printf("Digite a 4º nota: ");
+    nota4 = ler.nextByte();
+    ler.close();
+
+    int resultado = (nota1 + nota2 + nota3 + nota4) / 4;
+
+    System.out.println("A média final é: " + resultado);
   }
 }
